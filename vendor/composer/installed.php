5833af9aef819a469bc98ddc9933f87e7741b2f0
Yurii Petrovskyi <yurakarpaty@gmail.com>
2025-01-24 12:23:41 +0000

    added the vendor folders to .gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '43c9992c0b12e6f376620a44bb3e74f762d2f791',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '43c9992c0b12e6f376620a44bb3e74f762d2f791',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
